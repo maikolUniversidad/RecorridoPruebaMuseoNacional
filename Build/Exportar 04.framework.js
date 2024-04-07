93ea5c63197ceadab722f7ed055d8b2d6839a298
maikolUniversidad <112012566+maikolUniversidad@users.noreply.github.com>
2024-04-07 00:17:02 -0500

    Primer commit del proyecto de recorrido 360 en Unity

---
+++ Build/Exportar 04.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a89f9d853e2cab01e351de3c90ef61839c67c56c3b6dd63882c6a879d3012ba
+size 414779
---
+++ Build/Exportar 04.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c3bfd5a153d645c4500d322b2ab6c7691959e45bed4333dfed36d343f525987
+size 25469
